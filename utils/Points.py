68128f21132fcac5db04526d5ab4fe0b5b579883
PedroCimini <PedCMF18@gmail.com>
2024-10-04 18:48:17 -0300

    Improvement on legibility

--- utils/Points.py
+++ utils/Points.py
@@ -4,27 +4,49 @@ import pandas as pd
 # Pega todos os pontos dos jogos onde o jogador participou, 
 # e retorna apenas os pontos do próprio jogador
 
-def PlayerPoints(player_name, points_data, player_matches_data):
-    
-    Player_Points_Won = pd.DataFrame()
-    Player_Points_Lost = pd.DataFrame()
+def PlayerPoints(player_name, points_data, player_game_id):
 
-    for _,row in player_matches_data.iterrows():
-        Player1 = row['Player 1']
-        game_points_data = points_data[points_data['match_id'] == row['match_id']]
+    WonPoints = pd.DataFrame()
+    LostPoints = pd.DataFrame()
+    
+    for id in player_game_id:
+        game = points_data[points_data['match_id'] == id]
+        Player1 = game['Player 1'].unique()
 
         if(Player1 == player_name):
-            Player_points_data_won = game_points_data[game_points_data['PtWinner'] == 1]
-            Player_points_data_lost= game_points_data[game_points_data['PtWinner'] == 2]
+            PointsWon = game[game['PtWinner'] == 1]
+            PointsLost = game[game['PtWinner'] == 2]
         else:
-            Player_points_data_won  = game_points_data[game_points_data['PtWinner'] == 2]
-            Player_points_data_lost  = game_points_data[game_points_data['PtWinner'] == 1]
+            PointsWon = game[game['PtWinner'] == 2]
+            PointsLost = game[game['PtWinner'] == 1]
         
-        Player_Points_Won = pd.concat([Player_Points_Won,Player_points_data_won])
-        Player_Points_Lost = pd.concat([Player_Points_Lost,Player_points_data_lost])
+        WonPoints = pd.concat([WonPoints,PointsWon])
+        LostPoints = pd.concat([LostPoints,PointsLost])
+    
+    return WonPoints.reset_index(drop=True), LostPoints.reset_index(drop=True)
+
+
     
 
-    return Player_Points_Won,Player_Points_Lost
 
+def PlayerServes(player_name, points_data, player_game_id):
+    
+    Serves = pd.DataFrame()
+    Receives = pd.DataFrame()
+
+    for id in player_game_id:
+        game = points_data[points_data['match_id'] == id]
+        Player1 = game['Player 1'].unique()
+
+        if (Player1 == player_name):
+            Serving = game[game['Svr'] == 1]
+            Receiving = game[game['Svr'] == 2]
+        else:
+            Serving = game[game['Svr'] == 2]
+            Receiving = game[game['Svr'] == 1]
         
-        
+        Serves = pd.concat([Serves,Serving])
+        Receives = pd.concat([Receives,Receiving])
+    
+    return Serves.reset_index(drop=True), Receives.reset_index(drop=True)
+
--- utils/Tools.py
+++ utils/Tools.py
@@ -1,11 +1,11 @@
 
 
-def ResumeStats(data, columns):
+def ResumePointsStats(data, columns):
+    total = len(data)
     for column in columns:
-        aux = data[column].value_counts()
+        aux = data[data[column] == True]
 
-        true = aux.iloc[1]
-        total = aux.iloc[0] + aux.iloc[1]
+        true = len(aux)
 
         Percentage = true/total
 
@@ -14,3 +14,27 @@ def ResumeStats(data, columns):
 # Make a function that calculates the way the point was won
 
 # Make a function that specifically calculates serve stats
+
+def ResumeServeStats(data):
+    FirstIn = data[data['1stIn'] == True]
+    FirstOut = data[data['1stIn'] == False]
+
+    
+    SecondIn = FirstOut[FirstOut['2ndIn'] == True]
+
+   
+    FirstIn_Percent = len(FirstIn)/len(data)
+    SecondIn_Percent = len(SecondIn)/len(FirstOut)
+
+
+    print(f'1stIn percentage : {FirstIn_Percent}')
+    print(f'2ndIn percentage : {SecondIn_Percent}')
+
+
+def ReturnTerrain(data):
+    
+    Clay = data[data['Surface'] == 'Clay']
+    Grass = data[data['Surface'] == 'Grass']
+    Hard = data[data['Surface'] == 'Hard']
+
+    return Hard, Clay, Grass
